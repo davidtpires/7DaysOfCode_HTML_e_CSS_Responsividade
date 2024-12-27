5c90d9cd706fc8285d66c0da8aec4a24629500d4
David T. <davidt.info@gmail.com>
2024-12-27 12:22:05 -0300

    Day6/7: Cadastro de consultas

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const botaoAdicionar = document.querySelector('.button_adicionar');
 const buttonConsultaLocal = document.querySelector('.button_consulta-local');
 const buttonConsultaVideo = document.querySelector('.button_consulta-video');
 const blackscreen = document.querySelector('.blackscreen');
+const formularioContainer = document.querySelector('.formulario__container');
+const botaoCadastrarConsulta = document.querySelector('.button_cadastrar_consulta');
+const botaoFecharFormulario = document.querySelector('.formulario__fechar');
 
 
 blackscreen.addEventListener('click', () => {    
@@ -15,5 +18,15 @@ botaoAdicionar.addEventListener('click', () => {
     blackscreen.classList.add('blackscreen-checked');
     buttonConsultaLocal.classList.add('button_consulta-local-checked');
     buttonConsultaVideo.classList.add('button_consulta-video-checked');
-
 });
+
+botaoCadastrarConsulta.addEventListener('click', () => {
+    formularioContainer.classList.add('formulario__container-checked');
+    botaoCadastrarConsulta.classList.add('button_cadastrar_consulta-checked');
+});
+
+botaoFecharFormulario.addEventListener('click', () => {
+    formularioContainer.classList.remove('formulario__container-checked');
+    botaoCadastrarConsulta.classList.remove('button_cadastrar_consulta-checked');
+})
+
